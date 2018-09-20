ba3423d2d0b84925bcbd7bf18d34f6dc90606339
svc-student <svc-student@student.directory.jccc>
2018-09-20 17:46:58 -0500

    exersise2 start

---
+++ Exersise2/Assets/PlayerController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+  public Camera playerCamera;
+
+  private float mouseX;
+  private float mouseXRotationSpeed = 100;
+  private float mouseY;
+  private float mouseYRotationSpeed = 100;
+
+  private float maxView = 90.0F;
+  private float minView = -90.0F;
+
+  private void Start()
+  {
+    if (!CheckIfIsLocalPlayer())
+    {
+      this.enabled = false;
+    }
+  }
+  public float speed = 10f;
+  public override void OnStartLocalPlayer()
+  {
+    GetComponent<MeshRenderer>().material.color = Color.blue;
+    playerCamera.enabled = true;
+  }
+
+  void Update()
+  {
+    MovementControls();
+    CameraControls();
+
+  }
+
+  private void MovementControls()
+  {
+    Vector3 movementDirection = new Vector3();
+    if (Input.GetKey(KeyCode.W)) { movementDirection += transform.forward; }
+    if (Input.GetKey(KeyCode.A)) { movementDirection += -transform.right; }
+    if (Input.GetKey(KeyCode.S)) { movementDirection += -transform.forward; }
+    if (Input.GetKey(KeyCode.D)) { movementDirection += transform.right; }
+
+    transform.position += movementDirection.normalized * speed * Time.deltaTime;
+  }
+
+  private void CameraControls()
+  {
+    mouseX = Input.GetAxis("Mouse X");
+    transform.Rotate(Vector3.up, mouseX * mouseXRotationSpeed * Time.deltaTime);
+
+    mouseY = Input.GetAxis("Mouse Y");
+
+    float angleEulerLimit = playerCamera.transform.eulerAngles.x;
+
+    if (angleEulerLimit > 180)
+    {
+      angleEulerLimit -= 360;
+    }
+
+    if (angleEulerLimit < -180)
+    {
+      angleEulerLimit += 360;
+    }
+
+    float targetRotation = angleEulerLimit + mouseY * -mouseYRotationSpeed * Time.deltaTime;
+
+    if (targetRotation < maxView && targetRotation > minView)
+    {
+      playerCamera.transform.eulerAngles += new Vector3(mouseY * -mouseYRotationSpeed * Time.deltaTime, 0, 0);
+
+    } // end TARGET ROTATION
+
+  }
+
+  private bool CheckIfIsLocalPlayer()
+  {
+    if (isLocalPlayer)
+    {
+      return true;
+    }
+
+    return false;
+  }
+}
